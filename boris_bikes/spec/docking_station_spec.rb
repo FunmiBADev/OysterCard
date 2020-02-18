ca47bade558c870f608e7d47998e4a52ce7fce63
FunmiBADev <funmiba@hotmail.com>
2020-02-18 16:48:21 +0000

    Release Error message Pass if no bikes avavailable

--- boris_bikes/spec/docking_station_spec.rb
+++ boris_bikes/spec/docking_station_spec.rb
@@ -3,10 +3,24 @@ require "docking_station"
 describe DockingStation do
 it { is_expected.to respond_to :release_bike }
 		
-it ' releases working bikes' do
-	bike = subject.release_bike
-	expect(bike).to be_working
-	end
+# it ' releases working bikes' do
+# 	bike = subject.release_bike
+# 	expect(bike).to be_working
+# 	end
+
+describe "#release_bike" do
+
+it " raises an error when there are no bike available" do 
+	# lets not dock a bike 1st, rem Subject == DockingStation.new
+	expect {subject.release_bike}.to raise_error "No bikes available"
+end
+
+it 'releases a bike' do 
+	bike = Bike.new
+	subject.dock_bike(bike)
+	expect(subject.release_bike).to eq bike
+end
+end
 
 it { is_expected.to respond_to(:dock_bike).with(1).argument }
 
